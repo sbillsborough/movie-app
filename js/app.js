00896bacea0657f7b832ddb2df35a2cf7d5e8522
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-12-16 14:29:13 +0000

    standard api version complete

--- js/app.js
+++ js/app.js
@@ -3,25 +3,27 @@ var itemWrapper = document.querySelector("main");
 
 // out comes the matches array
 function displayMatches(matches) {
-  // clear the main html element out
   itemWrapper.innerHTML = "";
 
-  // loop over matches
-  for (var matchObj of matches) {
-    // insert adjacent HTML to a new div for every match
-    itemWrapper.insertAdjacentHTML(
-      "beforeend",
-      `
-    <div class="movie-item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url(${matchObj.image_url});">
-      <h3>${matchObj.title}</h3>
-      <p>
-      ${matchObj.description}
-      </p>
-      <a href="${matchObj.imdb_url}">View more details.</a>
-    </div>
-    `
-    );
+  if (!matches) {
+    itemWrapper.innerHTML = '<p class="no-search">No results found</p>';
+  } else {
+    for (var matchObj of matches) {
+      // insert adjacent HTML to a new div for every match
+      itemWrapper.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class="movie-item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+        url(${matchObj.Poster});">
+          <h3>${matchObj.Title}</h3>
+          <p>
+          ${matchObj.Year}
+          </p>
+          <a href="https://www.imdb.com/title/${matchObj.imdbID}">View more details.</a>
+        </div>
+        `
+      );
+    }
   }
 }
 
@@ -33,40 +35,58 @@ function getMovieData(event) {
 
   // checks if keycode is 13 (13 is the enter key)
   if (keyCode === 13 && searchText) {
-    var matches = [];
+    // var matches = [];
 
-    for (var movie of movieData) {
-      if (movie.title.toLowerCase().includes(searchText)) {
-        // push to matches array
-        matches.push(movie);
-      }
-    }
+    // for (var movie of movieData) {
+    //   if (movie.title.toLowerCase().includes(searchText)) {
+    //     // push to matches array
+    //     matches.push(movie);
+    //   }
+    // }
 
-    // fetch omdb data (fetch is like visitAddress())
     var responsePromise = fetch(
-      // returns a promise object
-      "https://www.omdbapi.com/?apikey=3fa32110&t=drive"
+      `https://www.omdbapi.com/?apikey=3fa32110&s=${searchText}`
     );
 
     function handleResponse(responseObj) {
       return responseObj.json();
     }
 
-    responsePromise
-      .then(handleResponse)
-      .then(function (data) {
-        console.log(data);
-        return "this is cool";
-      })
-      .then(function (huh) {
-        console.log(huh);
-        var test = "test";
-        console.log(test);
-      });
+    responsePromise.then(handleResponse).then(function (data) {
+      displayMatches(data.Search);
+      // console.log(data); data.search is our array of matches
+    });
+
+    // arrow function fetch request
+
+    // fetch("https://www.omdbapi.com/?apikey=3fa32110&t=drive")
+    //   .then((res) => res.json())
+    //   .then((data) => console.log(data));
+
+    // fetch omdb data (fetch is like visitAddress())
+    // var responsePromise = fetch(
+    //   // returns a promise object
+    //   "https://www.omdbapi.com/?apikey=3fa32110&t=drive"
+    // );
+
+    // function handleResponse(responseObj) {
+    //   return responseObj.json();
+    // }
+
+    // responsePromise
+    //   .then(handleResponse)
+    //   .then(function (data) {
+    //     console.log(data);
+    //     return "this is cool";
+    //   })
+    //   .then(function (huh) {
+    //     console.log(huh);
+    //     var test = "test";
+    //     console.log(test);
+    //   });
 
     // outside of for loop but inside if statement
     // in goes the matches array
-    displayMatches(matches);
   }
 }
 
